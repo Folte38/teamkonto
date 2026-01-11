2c9e1fe266f659e3a7271b5ff4bb1e2e512e15e8
Folte38 <38sainti@gmail.com>
2026-01-11 19:38:03 +0100

    Update script-main.js

--- script-main.js
+++ script-main.js
@@ -25,7 +25,8 @@ const WEEK_LABEL = "Woche 1";
 // =========================
 
 const goals = [
-  { name: "Bohrer V3", cost: 1000000000, current: 513000000 },
+  { name: "Bohrer V2", cost: 500000000, current: 0 },
+  { name: "Bohrer V2", cost: 500000000, current: 500000000 },
   { name: "Phils Bauhacke (Erledigt)", cost: 205000000, current: 205000000 }
 ];
 
@@ -153,6 +154,7 @@ document.getElementById("shotBtn").onclick = () => {
   const isShot = document.body.classList.contains("shot");
   renderStats(isShot);
 };
+
 
 
 
